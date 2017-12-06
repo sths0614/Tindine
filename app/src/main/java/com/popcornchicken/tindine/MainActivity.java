4a98bf0bff6891dae03e334a794bf9f6ec4694f4
Bryan Ho <6991ohnayrb@gmail.com>
2017-12-06 01:38:11 -0800

    update themes and formatting

--- app/src/main/java/com/popcornchicken/tindine/MainActivity.java
+++ app/src/main/java/com/popcornchicken/tindine/MainActivity.java
@@ -1,6 +1,5 @@
 package com.popcornchicken.tindine;
 
-import android.app.ActionBar;
 import android.app.Fragment;
 import android.content.Intent;
 import android.location.Geocoder;
@@ -9,6 +8,7 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.app.FragmentManager;
 import android.support.v13.app.FragmentPagerAdapter;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -163,6 +163,7 @@ public class MainActivity extends AppCompatActivity
                 if (previousPosition != position) {
                     previousPosition = position;
                     mViewPager.setCurrentItem(position);
+                    updateToolbarText(position);
                 }
 
                 return true;
@@ -199,19 +200,25 @@ public class MainActivity extends AppCompatActivity
         mFirebaseManager = new FirebaseManager(this);
         mFirebaseManager.attachInitialFirebaseListeners(userID, userCity);
         mFirebaseManager.attachFirebaseListeners(userID, userCity);
-
-//        try {
-//            Thread.sleep(3000);
-//        } catch (Exception e) {
-//            //swallows
-//        }
     }
 
-    private void updateToolbarText(CharSequence text) {
-        Log.d("bahhh", "got here in updateToolbarText");
-        ActionBar actionBar = getActionBar();
+    private void updateToolbarText(int index) {
+        ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
-            actionBar.setTitle("Hi");
+            switch (index) {
+                case 0:
+                    actionBar.setTitle("Sent Requests");
+                    break;
+                case 1:
+                    actionBar.setTitle("Claimed Requests");
+                    break;
+                case 2:
+                    actionBar.setTitle("Nearby Requests");
+                    break;
+                case 3:
+                    actionBar.setTitle("New Requests");
+                    break;
+            }
         }
     }
 
