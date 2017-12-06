c9e6cc849e15c0a326029df7704206bf5ad5c66f
Bryan Ho <6991ohnayrb@gmail.com>
2017-12-05 23:18:17 -0800

    Show images in list view

--- app/src/main/java/com/popcornchicken/tindine/RequestAdapter.java
+++ app/src/main/java/com/popcornchicken/tindine/RequestAdapter.java
@@ -1,6 +1,8 @@
 package com.popcornchicken.tindine;
 
 import android.content.Context;
+import android.net.Uri;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -10,30 +12,43 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class RequestAdapter extends ArrayAdapter<Request> {
     private ArrayList<Request> mRequests;
     private Context mContext;
+    ViewHolder holder;
 
-    public RequestAdapter(Context context, ArrayList<Request> requests){
+    public RequestAdapter(Context context, ArrayList<Request> requests) {
         super(context, 0, requests);
         mRequests = requests;
         mContext = context;
     }
 
-    private class ViewHolder{
+    private class ViewHolder {
         ImageView restaurantImage;
         TextView restaurantName;
         TextView restaurantAddress;
         TextView requestStatus;
     }
 
+    @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         // Get the request item for this position
-        Request request = getItem(position);
-        ViewHolder holder = null;
+        final Request request = getItem(position);
+        holder = null;
 
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_request, parent, false);
@@ -47,6 +62,37 @@ public class RequestAdapter extends ArrayAdapter<Request> {
         } else {
             holder = (ViewHolder) convertView.getTag();
         }
+
+        final StringBuilder stringBuilder = new StringBuilder();
+        final Handler handler = new Handler();
+        final Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                Picasso.with(getContext()).load(stringBuilder.toString()).into(holder.restaurantImage);
+            }
+        };
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                final HttpClient httpClient = new DefaultHttpClient();
+                final String placesUrl = "https://maps.googleapis.com/maps/api/place/details/json?placeid=" + request.getRequestData().getRestaurant().getRestaurantID() + "&key=AIzaSyCnTJyympLgv92-Siakx4piUr1GfeiQd8I";
+                final HttpGet httpGet = new HttpGet(placesUrl);
+                try {
+                    HttpResponse response = httpClient.execute(httpGet);
+                    String serverResponse = EntityUtils.toString(response.getEntity());
+                    JSONObject jsonObject = new JSONObject(serverResponse);
+                    JSONArray jsonArray = jsonObject.getJSONObject("result").getJSONArray("photos");
+                    String photoReference = jsonArray.getJSONObject(0).getString("photo_reference");
+
+                    String photoReferenceUrl = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=" + photoReference + "&key=AIzaSyCnTJyympLgv92-Siakx4piUr1GfeiQd8I";
+                    stringBuilder.append(photoReferenceUrl);
+                    handler.postDelayed(runnable, 0);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }).start();
 
         holder.restaurantName.setText(request.getRequestData().getRestaurant().getRestaurantName());
         holder.restaurantAddress.setText(request.getRequestData().getRestaurant().getRestaurantAddress());
